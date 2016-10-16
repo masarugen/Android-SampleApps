5d26a30e78445ccbbab8d559dae00a74e0246442
hashizume.masaru <hashizume.masaru@gmail.com>
2016-10-16 15:41:33 +0900

    add module

---
+++ samplejar/src/main/java/to/tyo/sample/jar/Test.java
@@ -0,0 +1,11 @@
+package to.tyo.sample.jar;
+
+/**
+ * Created by hashizume on 2016/10/16.
+ */
+
+public class Test {
+    public static void hello() {
+        System.out.println("hello");
+    }
+}
---
+++ samplejar/src/test/java/TestProcessor.java
@@ -0,0 +1,16 @@
+import to.tyo.sample.jar.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by hashizume on 2016/10/16.
+ */
+
+public class TestProcessor {
+
+    @org.junit.Test
+    public void testTest() throws Exception {
+        Test.hello();
+        fail();
+    }
+}
