5acf7fbb83ae1ff4d3e8bde13d7655f38727a8cd
hashizume.masaru <hashizume.masaru@gmail.com>
2016-10-18 02:14:01 +0900

    fix

--- .travis_scripts/pullrequest.sh
+++ .travis_scripts/pullrequest.sh
@@ -1,41 +1,53 @@
 #!/bin/bash -xe
-HUB="2.2.0"
 
-# 認証情報を設定する
-mkdir -p "$HOME/.config"
-set +x
-echo "https://${GH_TOKEN}:@github.com" > "$HOME/.config/git-credential"
-echo "github.com:
-- oauth_token: $GH_TOKEN
-  user: $GH_USER" > "$HOME/.config/hub"
-unset GH_TOKEN
-set -x
+createPR() {
+  echo "===== start createPR ====="
 
-# Gitを設定する
-git config --global user.name  "${GH_USER}"
-git config --global user.email "${GH_USER}@users.noreply.github.com"
-git config --global hub.protocol "https"
-git config --global credential.helper "store --file=$HOME/.config/git-credential"
+  HUB="2.2.0"
 
-# hubをインストールする
-curl -LO "https://github.com/github/hub/releases/download/v$HUB/hub-linux-amd64-$HUB.tar.gz"
-tar -C "$HOME" -zxf "hub-linux-amd64-$HUB.tar.gz"
-export PATH="$PATH:$HOME/hub-linux-amd64-$HUB"
+  # 認証情報を設定する
+  mkdir -p "$HOME/.config"
+  set +x
+  echo "https://${GH_TOKEN}:@github.com" > "$HOME/.config/git-credential"
+  echo "github.com:
+  - oauth_token: $GH_TOKEN
+    user: $GH_USER" > "$HOME/.config/hub"
+  unset GH_TOKEN
+  set -x
 
-# リポジトリに変更をコミットする
-hub clone "Android-SampleApps" _
-cd _
-BRANCH_NAME="branch_name_"`date "+%Y-%m-%d_%H-%M-%S"`
-hub checkout -b $BRANCH_NAME
-## ファイルを変更する ##
-echo "hoge" > hoge.txt
-hub add .
-if hub commit -m "add hoge" ; then
-  # Pull Requestを送る
-  hub push origin $BRANCH_NAME
-  hub pull-request -m "auto update pull request"
+  # Gitを設定する
+  git config --global user.name  "${GH_USER}"
+  git config --global user.email "${GH_USER}@users.noreply.github.com"
+  git config --global hub.protocol "https"
+  git config --global credential.helper "store --file=$HOME/.config/git-credential"
+
+  # hubをインストールする
+  curl -LO "https://github.com/github/hub/releases/download/v$HUB/hub-linux-amd64-$HUB.tar.gz"
+  tar -C "$HOME" -zxf "hub-linux-amd64-$HUB.tar.gz"
+  export PATH="$PATH:$HOME/hub-linux-amd64-$HUB"
+
+  # リポジトリに変更をコミットする
+  hub clone "Android-SampleApps" _
+  cd _
+  BRANCH_NAME="branch_name_"`date "+%Y-%m-%d_%H-%M-%S"`
+  hub checkout -b $BRANCH_NAME
+  ## ファイルを変更する ##
+  echo "hoge" > hoge.txt
+  hub add .
+  if hub commit -m "add hoge" ; then
+    # Pull Requestを送る
+    hub push origin $BRANCH_NAME
+    hub pull-request -m "auto update pull request"
+  else
+    echo "There no updates"
+  fi
+
+  cd ..
+}
+
+if [ `echo $TRAVIS_BRANCH | grep brach_name` ]; then
+  # travis ciから生成されたprの場合は、再度prを作成しない
+  echo $TRAVIS_BRANCH
 else
-  echo "There no updates"
+  createPR()
 fi
-
-cd ..
